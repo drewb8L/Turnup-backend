7f7a57cd6b0fb91a2c15791aee714a94d33cde4a
drewb8L <104442990+drewb8L@users.noreply.github.com>
2022-11-08 13:12:45 -0600

    Added user types to UserDTO

--- Turnup/DTOs/UserDTO.cs
+++ Turnup/DTOs/UserDTO.cs
@@ -1,7 +1,11 @@
+using Turnup.Entities;
+
 namespace Turnup.DTOs;
 
 public class UserDTO
 {
     public string Username { get; set; }
     public string Password { get; set; }
+
+    public UserType UserType { get; set; }
 }
