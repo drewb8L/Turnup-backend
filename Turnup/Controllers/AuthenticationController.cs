9a1f966edc6b7feacccb0f8fbf810fc9b6320b45
ababcock013 <ababcock013@outlook.com>
2022-11-25 14:11:31 -0600

    refactored jwt role claim

--- Turnup/Controllers/AuthenticationController.cs
+++ Turnup/Controllers/AuthenticationController.cs
@@ -141,6 +141,7 @@ public class AuthenticationController : ControllerBase
             {
                 new Claim("Id", user.Id),
                 new Claim(ClaimTypes.Role, user.Role ),
+                new Claim("Role",user.Role),
                 new Claim(JwtRegisteredClaimNames.Sub, user.Email),
                 new Claim(JwtRegisteredClaimNames.Email, value: user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
